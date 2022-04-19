b27da91dd435adc57d4933f61b26c1262a209ee1
Semyon Dyachenko <semyondyachenko@gmail.com>
2022-04-19 19:08:01 +0300

    jkjkkj

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,35 +1,52 @@
 import Link from "next/link";
+import '../styles/header.module.css'
 
 export function MainLayout({ children }) {
     return (
         <>
-            <nav>
-                <Link href={'/posts'}><a>Home</a></Link>
-                <Link href={'/posts'}><a>About</a></Link>
-                <Link href={'/posts'}><a>Posts</a></Link>
-                <Link href={'/posts'}><a>Products</a></Link>
-                <Link href={'/posts'}><a>for Sale</a></Link>
-            </nav>
 
+            <header>
+                <nav>
+                    <Link href={'/posts'}><a>Home</a></Link>
+                    <Link href={'/posts'}><a>About</a></Link>
+                    <Link href={'/posts'}><a>Posts</a></Link>
+                    <Link href={'/posts'}><a>Products</a></Link>
+                    <Link href={'/posts'}><a>for Sale</a></Link>
+                </nav>
+
+            </header>
             <main>
                 {children}
             </main>
 
             <style jsx> {`
+
+                header {
+                  width:100%;
+                  height: auto;
+                  float:left;
+                  box-sizing: border-box;
+                }
+                
+                
                 nav {
+                margin-top:100px;
                 position:fixed;
                 height: 60px;
                 left:0;
                 top:0;
                 right:0;
-                background:blue;
                 display:flex;
-                justify-content: space-around;
+                justify-content: center;
                 align-items: center;
                 }
                 
                 a {
-                color:white;
+                display: block;
+                margin-left:20px;
+                margin-right: 20px;
+                color:black;
+                font-size:18px;
                 }
             `
             }
